0a2c7723623b2d60ff68b57c0152eaee9aebbd1e
amolthakare <100188653+amolthakare@users.noreply.github.com>
2022-03-30 11:03:52 +0530

    my first commit

---
+++ Data/bodyData.js
@@ -0,0 +1,205 @@
+var bodyProducts=[
+    {
+        title:"Vitamin C Underarm Scrub with Vitamin C and Turmeric for Brighter Underarms – 50 g",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-underarm-scrub.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹299",
+        rating:"5.0★",
+    },
+    {
+        title:"Vitamin C Underarm Mask with Vitamin C & Turmeric for Brighter Underarms - 50 g",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-underarm-mask.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹299",
+        rating:"5.0★",
+    },
+    {
+        title:"Vitamin C Underarm Mask with Vitamin C & Turmeric for Brighter Underarms - 50 g",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-underarm-cream_1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹299",
+        rating:"5.0★",
+    },
+    {
+        title:"Skin Radiance Body Care Kit - Ubtan Body Lotion & Vitamin C Body Lotion - (400 ml each)",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_and_ubtan.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹799",
+        price:"₹518",
+        rating:"4.9★"
+    },
+    {
+        title:"Skin Refreshing & Soothing Body Care Kit - Rose Body Lotion + Vitamin C Body Lotion - (400 ml)",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_and_rose.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹799",
+        price:"₹518",
+        rating:"4.9★",
+    },
+    {
+        title:"Skin Nourishing Body Care Combo - Ubtan Body Lotion + Rose Body Lotion  - (400 ml)",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan_and_rose.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹799",
+        price:"₹518",
+        rating:"4.9★",
+    },
+    {
+        title:"Vitamin C Body Lotion with Vitamin C & Honey for Radiant Skin - 400 ml   (Pack of 2)",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹799",
+        price:"₹518",
+        rating:"4.9★",
+    },
+    {
+        title:"Ubtan Body Lotion with Turmeric & Kokum Butter for Glowing Skin - 400 ml  (Pack of 2)",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan_body_lotion_7.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹799",
+        price:"₹518",
+        rating:"4.9★",
+    },
+    {
+        title:"Rose Body Lotion with Rose Water & Milk for Deep Hydration (400 ml) - Pack of 2",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/r/o/rose_body_lotion_7.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹799",
+        price:"₹518",
+        rating:"4.9★",
+    },
+    {
+        title:"Mamaearth ME Eau De Parfum For a Fragrance As Unique As You - 50 ml",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/p/e/perfume-1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹699",
+        rating:"4.9★",
+    },
+    {
+        title:"Ubtan Nourishing Hair Removal Kit With Turmeric & Saffron for Hair Removal & Growth Reduction - 100+50 g",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan-hair-removal-kit.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹449",
+        rating:"4.9★",
+    },
+    {
+        title:"Rose Body Lotion with Rose Water and Milk For Deep Hydration - 400ml",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/r/o/rose_body_lotion_1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹399",
+        rating:"4.9★",
+    },
+    {
+        title:"Vitamin C Body Lotion with Vitamin C and Honey for Radiant Skin - 400 ml ",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion_1_1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹299",
+        rating:"4.9★",
+    },
+    {
+        title:"Ubtan Body Wash With Turmeric and Saffron for Glowing Skin - 300 ml",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan-body-wash-1_c7usn1sqjckdbdg5.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹299",
+        rating:"4.9★",
+    },
+    {
+        title:"Ubtan Nourishing Cold Cream with Turmeric & Saffron for Glowing Moisturization – 200 g",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan-cold-cream-1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹299",
+        rating:"4.9★",
+    },
+    {
+        title:"Vitamin C Nourishing Cold Cream with Vitamin C & Honey for Illuminating Moisturization - 200 g",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-cold-cream-1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹299",
+        rating:"4.9★",
+    },
+    {
+        title:"CoCo Nourishing Cold Cream With Coffee and Vitamin E For Rich Moisturization - 200 g",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/c/o/coco-cold-cream-1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹299",
+        rating:"4.9★",
+    },
+    {
+        title:"CoCo Body Lotion With Coffee and Cocoa for Intense Moisturization - 400ml ",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/c/o/coco_body_lotion_1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹399",
+        rating:"4.8★",
+    },
+    {
+        title:"Ubtan Body Lotion with Turmeric and Kokum Butter for Glowing Skin - 400 ml",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan_body_lotion_1_1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹399",
+        rating:"4.8★",
+    },
+    {
+        title:"Vitamin C Hand Cream with Vitamin C and Shea Butter for Intense Moisturization",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_hand_cream_1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹349",
+        rating:"4.9★",
+    },
+    {
+        title:"Rose Body Lotion with Rose Water & Milk for Deep Hydration (400 ml) - Pack of 2",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/r/o/rose_body_lotion_7.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹799",
+        price:"₹518",
+        rating:"4.9★",
+    },
+    {
+        title:"Mamaearth ME Eau De Parfum For a Fragrance As Unique As You - 50 ml",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/p/e/perfume-1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"",
+        price:"₹699",
+        rating:"4.9★",
+    },
+    {
+        title:"Skin Radiance Body Care Kit - Ubtan Body Lotion & Vitamin C Body Lotion - (400 ml each)",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_and_ubtan.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹799",
+        price:"₹518",
+        rating:"4.9★"
+    },
+    {
+        title:"Charcoal Nourishing Bathing Soap With Charcoal and Mint for Deep Cleansing - 5x75g",
+        imageurl:"https://honasa-mamaearth-production.imgix.net/c/h/charcoal-soap_1.jpg?auto=compress&fit=scale&w=400&h=400",
+        strike_price:"₹375",
+        price:"₹299",
+        rating:"4.8★",
+    },
+
+];
+
+localStorage.setItem("bodyProducts",JSON.stringify(bodyProducts));
+
+// var bodyProducts=JSON.parse(localStorage.getItem("bodyProducts"));
+
+
+    bodyProducts.map(function (elem,index,array){
+        
+        var mainDiv=document.createElement("div");
+    
+        var img=document.createElement("img");
+        img.setAttribute("src",elem.imageurl);
+
+        var title=document.createElement("p");
+        title.innerText=elem.title;
+
+        var rating=document.createElement("p");
+        rating.innerText=elem.rating;
+
+        var div=document.createElement("div");
+
+        var price=document.createElement("p");
+        price.innerText=elem.price;
+
+        var strike=document.createElement("s");
+        strike.innerText=elem.strike_price;
+
+        var btn=document.createElement("button");
+        btn.innerText="ADD TO CART";
+        
+        div.append(price,strike);
+        mainDiv.append(img,title,rating,div,btn);
+        document.querySelector("#products").append(mainDiv);
+    });
