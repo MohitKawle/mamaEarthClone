7c52c8fd14f6e13f1896f87104b189abbb1f134f
amolthakare <100188653+amolthakare@users.noreply.github.com>
2022-04-01 13:56:59 +0530

    u

--- Data/bodyData.js
+++ Data/bodyData.js
@@ -238,6 +238,15 @@ const appendBody= (data,where,value) =>{
 
         let btn = document.createElement("button");
         btn.innerText = "ADD TO CART"
+    //     $(btn).click(function () {
+    //       $('.alert').addClass("show");
+    //       $('.alert').removeClass("hide");
+    //       $('.alert').addClass("showAlert");
+    //       setTimeout(function () {
+    //           $('.alert').removeClass("show");
+    //           $('.alert').addClass("hide");
+    //       }, 1000);
+    //   });
         btn.setAttribute("id","addTocart")
         btn.addEventListener("click",()=>{
             addTocart(elem)
